45157c6b6182b3e4ac73a18d79630a78d660f7f7
Niexiuwen <1242677607@qq.com>
2023-03-21 20:08:34 +0800

    2

--- demo01.py
+++ demo01.py
@@ -15,15 +15,15 @@ print(23,"jyh",245)
 a = "张三"  #把张三这个值赋值给名字叫a的这个变量
 print(a)
 """
-#a = input("请输入")
-#print(a)
-# print(type(2))
-# print(type([]))
-# print(type(2.33))
-# a = '12'
-# print(a)
-# a = float(2)
-# print(a)
+a = input("请输入")
+print(a)
+print(type(2))
+print(type([]))
+print(type(2.33))
+a = '12'
+print(a)
+a = float(2)
+print(a)
 
 # a = 'hdnjsdsbsviu  wvvjbvw'
 # print(len(a))
