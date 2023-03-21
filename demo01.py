f7b6b3ac9fcc90f9d7ccebb3a9e7a3a34f3d8f61
Niexiuwen <1242677607@qq.com>
2023-03-21 20:00:50 +0800

    第一次上传到github

---
+++ demo01.py
@@ -0,0 +1,34 @@
+"""
+print("hello world!")  #字符串
+print(2333)    #整数
+print(2.33)    #小数
+print(True)    #布尔值
+print(())      #元组
+print([])      #数组
+print({})      #字典
+"""
+
+"""
+print(2.33/2)
+print("ha"+"lili")
+print(23,"jyh",245)
+a = "张三"  #把张三这个值赋值给名字叫a的这个变量
+print(a)
+"""
+#a = input("请输入")
+#print(a)
+# print(type(2))
+# print(type([]))
+# print(type(2.33))
+# a = '12'
+# print(a)
+# a = float(2)
+# print(a)
+
+# a = 'hdnjsdsbsviu  wvvjbvw'
+# print(len(a))
+a = input("输入1:")
+b = input("输入2:")
+# c = len(a)+len(b)
+# print(int(len(a))+int(len(b)))
+print(len(a)+len(b))
